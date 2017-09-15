a7aebe3b361f20c0be40fbd0a9eb5379bd34dbd2
Jason Adkison <jadkison@gmail.com>
2017-09-15 09:19:49 -0500

    fixes redux devtools setup

--- app/js/store.js
+++ app/js/store.js
@@ -70,7 +70,9 @@ const reducers = combineReducers({
 
 const middleware = applyMiddleware(thunk);
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default createStore(
   reducers,
-  compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  composeEnhancers(middleware)
 );
